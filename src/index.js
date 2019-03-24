87524ed6df49b8d1bc303c97a9c05af6e6c13343
korol <cx.korol@gmail.com>
2019-03-24 11:54:44 +0300

    Brackets. 17/20 Tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var parentheses = "[]{}()1234",
+    stack = [],
+    i, character, bracePosition;
+
+  for(i = 0; character = str[i]; i++) {
+    bracePosition = parentheses.indexOf(character);
+
+    if(bracePosition === -1) {
+      continue;
+    }
+
+    if(bracePosition % 2 === 0) {
+      stack.push(bracePosition + 1);
+    } else {
+      if(stack.length === 0 || stack.pop() !== bracePosition) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
